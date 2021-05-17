e65768231c2732bbca68bd6b9969bd986b3b3f55
EtbStic <78250384+EtbStic@users.noreply.github.com>
2021-05-17 22:49:56 +0300

    init

--- lib/screens/tabScreen.dart
+++ lib/screens/tabScreen.dart
@@ -10,9 +10,9 @@ class TabScreen extends StatefulWidget {
 class _TabScreenState extends State<TabScreen> {
   int selectedPageIndex = 0;
 
-  List<Widget> selectedPage = [
-    CategoriesScreen(),
-    FavoriteScreen(),
+  List<Map<String, Object>> selectedPage = [
+    {'page': CategoriesScreen(), 'title': "Meal Category"},
+    {'page': FavoriteScreen(), 'title': "Your Favorite"},
   ];
 
   void pageSelected(int index) {
@@ -25,17 +25,24 @@ class _TabScreenState extends State<TabScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Meal"),
+        title: Text(selectedPage[selectedPageIndex]['title']),
       ),
-      body: selectedPage[selectedPageIndex],
+      body: selectedPage[selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: pageSelected,
+        backgroundColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.white,
+        currentIndex: selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
         items: [
           BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
             icon: Icon(Icons.list_alt_outlined),
             label: "Category",
           ),
           BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
             icon: Icon(Icons.star_border_sharp),
             label: "Favorite",
           ),
